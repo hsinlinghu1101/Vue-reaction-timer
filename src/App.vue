f1b34b0bca3991033ece838e56c188da986916b5
Hsin Ling Hu <hsinkava11@gmail.com>
2021-02-24 23:17:53 -0800

    set up project

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <h1>My Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">play</button>
+  <Block v-if='isPlaying' :delay="delay"/>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
 
+import Block from './components/Block.vue'
 export default {
   name: "App",
-  components: {
-    HelloWorld
+  components: {Block},
+  data(){
+    return{
+      isPlaying:false,
+      delay: null,
+    }
+  },
+  methods:{
+    start(){
+      this.delay = 2000 + Math.random()* 5000
+      this.isPlaying=true
+    }
   }
 };
 </script>
 
 <style>
-#app {
+#app{
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
